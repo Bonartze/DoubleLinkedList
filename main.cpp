178cc7ba7e5bb4308e871f684c95882188c5218a
Ivan <iokirilin@mail.ru>
2023-06-11 19:17:50 +0300

    Initial

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include "LinkedList.h"
 
 int main() {
-    LinkedList<int> a = LinkedList<int>{5};
-    a.add_end(14);
-    a.add_end(15);
-  //  a.add_beg(40);
- //   a.add_beg(45);
-   a.delete_end();
-   a.delete_begin();
-   a.delete_begin();
-   a.delete_begin();
-    std::cout << a;
+    try {
+        LinkedList<int> a = LinkedList<int>{5};
+        a.add_end(14);
+        a.add_end(15);
+        //  a.add_beg(40);
+        //   a.add_beg(45);
+        a.delete_end();
+        a.delete_begin();
+        a.delete_begin();
+        a.delete_begin();
+        std::cout << a;
+    }
+    catch (NullException &ex) {
+        std::cout << ex.what();
+    }
 }
